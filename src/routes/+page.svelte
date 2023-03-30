18b43e6b34def075e30de97e445675f481ef583a
Laila Dyer <laila.annika.dyer@gmail.com>
2023-03-30 17:53:12 +0300

    add Tailwind css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,15 @@
-<h1>Laila's corner of the internet</h1>
+<script lang="ts">
 
-<style>
+</script>
+
+<div class="bg-pink-300">
+    <h2 class="m-3 text-center">Laila's corner of the internet</h2>
+</div>
+
+<style lang="postcss">
     :global(body) {
         font-family: Capriola;
+        margin: 0;
+        background: theme(colors.pink.200)
     }
 </style>
