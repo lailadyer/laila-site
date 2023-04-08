8123f8fd694657c5126db142c61128ac64d78ac0
Laila Dyer <laila.annika.dyer@gmail.com>
2023-04-08 20:46:58 +0300

    add a better style to the notes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-    import noteBackground from "$lib/images/backgrounds/note-background.png"
-
     const loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     let loremIpsumArray: string[] = [];
     for (let index = 0; index < 40; index++) {
@@ -8,14 +6,16 @@
     }
 </script>
 
-<div class="bg-pink-300 m-8 text-2xl font-bold">
-    <h2 class="p-4 text-center ">Laila's corner of the internet</h2>
+<div class="bg-pink-300 m-8 text-2xl font-bold rounded-lg">
+    <h2 class="p-4 text-center text-shadow shadow-pink-200">Laila's corner of the internet</h2>
 </div>
-{#each loremIpsumArray as p}
-<div class="m-8 bg-repeat bg-contain shadow" style="background-image: url({noteBackground})">
-    <p class="p-8 opacity-100">{p}</p>
+<div class="flex flex-row flex-wrap justify-center">
+    {#each loremIpsumArray as p}
+        <div class="m-8 bg-blend-luminosity bg-pink-300 shadow-md max-w-lg bg-contain rounded-xl bg-repeat bg-[url('$lib/images/backgrounds/note-background.jpg')]">
+            <p class="p-8 text-shadow shadow-pink-200">{p}</p>
+        </div>
+    {/each}
 </div>
-{/each}
 
 <style>
     :global(body) {
