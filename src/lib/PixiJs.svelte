d17db9be2f2b960edd27b43df6f224919d332cb7
Laila Dyer <laila.annika.dyer@gmail.com>
2023-12-11 21:50:02 +0200

    Upgrade packages

--- src/lib/PixiJs.svelte
+++ src/lib/PixiJs.svelte
@@ -1,7 +1,33 @@
 <script lang="ts">
+import { Application, Circle, Sprite, Texture } from 'pixi.js'
+import { onMount } from 'svelte';
+let canvas: HTMLCanvasElement;
+
+onMount(() => {
+    const app = new Application({
+      width: 800,
+      height: 600,
+      backgroundColor: 0x1099bb,
+      view: canvas,
+    })
+
+    const stage = app.stage;
+
+    const rect = Sprite.from(Texture.WHITE);
+    rect.tint = 0x000000;
+    rect.width = 200;
+    rect.height = 200;
+    // rect.eventMode = "auto";
+    stage.addChild(rect);
+    rect.addEventListener("pointerdown", (event) => {
+        console.log("moved")
+        rect.tint = 0xffffff;
+    })
+    
+    app.ticker.add((dtMs) => {
+    })
+})
 
 </script>
 
-<div class="p-4">
-    This will house pixijs in the future :)
-</div>
+<canvas class="p-4" bind:this={canvas}/>
